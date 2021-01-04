0fbecafff270f971114ae04b677c33f33c02819d
Zoey <zoey.spessanha@zeetech.io>
2021-01-04 16:40:57 -0300

    added dialyzer

--- mix.exs
+++ mix.exs
@@ -43,6 +43,7 @@ defmodule Bank.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:git_hooks, "~> 0.5.0", only: [:test, :dev], runtime: false}
     ]
