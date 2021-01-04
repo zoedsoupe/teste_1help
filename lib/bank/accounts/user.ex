9d5fbc50068ce21d9eb370e4ec8985f3e2685dfa
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-01-04 20:51:37 -0300

    initial user and fixed extended schema

---
+++ lib/bank/accounts/user.ex
@@ -0,0 +1,92 @@
+defmodule Bank.Accounts.User do
+  @moduledoc """
+  Entity that execute authenticated actions
+  """
+
+  use Bank.Changeset, command: true
+
+  use Bank.Schema, expose: true
+
+  @fields ~w(balance cnpj confirmed? cpf mobile email first_name last_name password password_confirmation)a
+
+  # cpf and cnpj are also required
+  # but the check_constraint/3 will validate both
+  # both password and password_confirmation too
+  # however maybe_execute_command will validate they
+  @required_fields ~w(first_name last_name email mobile)
+
+  @simple_filters ~w(first_name email cpf cnpj)a
+  @simple_sortings ~w(first_name email cpf cnpj inserted_at)a
+
+  @exposed_fields ~w(first_name last_name email)
+
+  schema "users" do
+    field :balance, :integer
+    field :cnpj, :string
+    field :confirmed?, :boolean, default: false
+    field :cpf, :string
+    field :email, :string
+    field :first_name, :string
+    field :last_name, :string
+    field :mobile, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs, commands \\ []) do
+    user
+    |> cast(attrs, @fields)
+    |> validate_required(@required_fields)
+    |> check_constraint([:cpf, :cnpj], name: :cpf_or_cnpj)
+    |> remove_whitespaces(:first_name)
+    |> remove_whitespaces(:last_name)
+    |> capitalize_all_words(:first_name)
+    |> capitalize_all_words(:last_name)
+    |> remove_whitespaces(:email)
+    |> downcase(:email)
+    |> validate_email(:email)
+    |> validate_phone(:mobile)
+    |> validate_length(:password, min: 10)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:cpf)
+    |> unique_constraint(:cnpj)
+    |> unique_constraint(:email)
+    |> maybe_execute_command(commands, :check_password)
+    |> maybe_execute_command(commands, :set_password)
+  end
+
+  defp execute_command(changeset, :check_password) do
+    changeset
+    |> validate_required([:password])
+    |> validate_required([:password_confirmation])
+    |> case do
+      %Ecto.Changeset{valid?: true} ->
+        changeset.changes.password
+        |> Argon2.verify_pass(changeset.data.password_hash)
+        |> case do
+          true -> changeset
+          false -> changeset |> add_error(:password, "invalid password")
+        end
+
+      invalid_changeset ->
+        invalid_changeset
+    end
+  end
+
+  defp execute_command(changeset, :set_password) do
+    changeset
+    |> validate_required([:password])
+    |> validate_required([:password_confirmation])
+    |> case do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Argon2.hash_pwd_salt(pass))
+
+      invalid_changeset ->
+        invalid_changeset
+    end
+  end
+end
---
+++ test/bank/accounts_test.exs
@@ -0,0 +1,125 @@
+defmodule Bank.AccountsTest do
+  use Bank.DataCase
+
+  alias Bank.Accounts
+
+  describe "users" do
+    alias Bank.Accounts.User
+
+    @valid_attrs %{
+      balance: 42,
+      cnpj: "some cnpj",
+      confirmed?: true,
+      cpf: "some cpf",
+      email: "some email",
+      first_name: "some first_name",
+      last_name: "some last_name",
+      new_password: "some new_password",
+      new_password_confirmation: "some new_password_confirmation",
+      password: "some password",
+      password_confirmation: "some password_confirmation",
+      password_hash: "some password_hash"
+    }
+    @update_attrs %{
+      balance: 43,
+      cnpj: "some updated cnpj",
+      confirmed?: false,
+      cpf: "some updated cpf",
+      email: "some updated email",
+      first_name: "some updated first_name",
+      last_name: "some updated last_name",
+      new_password: "some updated new_password",
+      new_password_confirmation: "some updated new_password_confirmation",
+      password: "some updated password",
+      password_confirmation: "some updated password_confirmation",
+      password_hash: "some updated password_hash"
+    }
+    @invalid_attrs %{
+      balance: nil,
+      cnpj: nil,
+      confirmed?: nil,
+      cpf: nil,
+      email: nil,
+      first_name: nil,
+      last_name: nil,
+      new_password: nil,
+      new_password_confirmation: nil,
+      password: nil,
+      password_confirmation: nil,
+      password_hash: nil
+    }
+
+    def user_fixture(attrs \\ %{}) do
+      {:ok, user} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Accounts.create_user()
+
+      user
+    end
+
+    test "list_users/0 returns all users" do
+      user = user_fixture()
+      assert Accounts.list_users() == [user]
+    end
+
+    test "get_user!/1 returns the user with given id" do
+      user = user_fixture()
+      assert Accounts.get_user!(user.id) == user
+    end
+
+    test "create_user/1 with valid data creates a user" do
+      assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
+      assert user.balance == 42
+      assert user.cnpj == "some cnpj"
+      assert user.confirmed? == true
+      assert user.cpf == "some cpf"
+      assert user.email == "some email"
+      assert user.first_name == "some first_name"
+      assert user.last_name == "some last_name"
+      assert user.new_password == "some new_password"
+      assert user.new_password_confirmation == "some new_password_confirmation"
+      assert user.password == "some password"
+      assert user.password_confirmation == "some password_confirmation"
+      assert user.password_hash == "some password_hash"
+    end
+
+    test "create_user/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
+    end
+
+    test "update_user/2 with valid data updates the user" do
+      user = user_fixture()
+      assert {:ok, %User{} = user} = Accounts.update_user(user, @update_attrs)
+      assert user.balance == 43
+      assert user.cnpj == "some updated cnpj"
+      assert user.confirmed? == false
+      assert user.cpf == "some updated cpf"
+      assert user.email == "some updated email"
+      assert user.first_name == "some updated first_name"
+      assert user.last_name == "some updated last_name"
+      assert user.new_password == "some updated new_password"
+      assert user.new_password_confirmation == "some updated new_password_confirmation"
+      assert user.password == "some updated password"
+      assert user.password_confirmation == "some updated password_confirmation"
+      assert user.password_hash == "some updated password_hash"
+    end
+
+    test "update_user/2 with invalid data returns error changeset" do
+      user = user_fixture()
+      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
+      assert user == Accounts.get_user!(user.id)
+    end
+
+    test "delete_user/1 deletes the user" do
+      user = user_fixture()
+      assert {:ok, %User{}} = Accounts.delete_user(user)
+      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
+    end
+
+    test "change_user/1 returns a user changeset" do
+      user = user_fixture()
+      assert %Ecto.Changeset{} = Accounts.change_user(user)
+    end
+  end
+end
