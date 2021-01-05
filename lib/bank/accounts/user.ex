98b369a99c607457b97aafde8f34c1e2b2a0adeb
Zoey <zoey.spessanha@zeetech.io>
2021-01-04 23:29:32 -0300

    cpf and cnpj validation

--- lib/bank/accounts/user.ex
+++ lib/bank/accounts/user.ex
@@ -8,6 +8,7 @@ defmodule Bank.Accounts.User do
 
   alias Bank.Accounts.UserConfirmation, as: Confirmation
   alias Bank.Accounts.UserPassRecovery, as: PassRecovery
+  alias Bank.Common.Formats
 
   @fields ~w(balance cnpj confirmed? cpf mobile email first_name last_name password password_confirmation)a
 
@@ -21,6 +22,9 @@ defmodule Bank.Accounts.User do
   @simple_sortings ~w(first_name email cpf cnpj inserted_at)a
 
   @exposed_fields ~w(first_name last_name email)
+
+  @cpf_format Formats.cpf()
+  @cnpj_format Formats.cnpj()
 
   schema "users" do
     field :balance, :integer, default: 0
@@ -57,11 +61,113 @@ defmodule Bank.Accounts.User do
     |> validate_phone(:mobile)
     |> validate_length(:password, min: 10)
     |> validate_confirmation(:password)
+    |> validate_change(:cpf, &validate_cpf/2)
+    |> validate_change(:cnpj, &validate_cnpj/2)
     |> unique_constraint(:cpf)
     |> unique_constraint(:cnpj)
     |> unique_constraint(:email)
     |> maybe_execute_command(commands, :check_password)
     |> maybe_execute_command(commands, :set_password)
+  end
+
+  defp is_digit(ch) do
+    "0" <= ch && ch >= "9"
+  end
+
+  defp cpf_verifying_digit_parse(digits, amount) do
+    digit =
+      digits
+      |> Enum.zip(amount..2)
+      |> Enum.reduce(0, fn {d, i}, acc -> d * i + acc end)
+      |> rem(11)
+      |> case do
+        rem when rem > 9 -> 0
+        rem -> rem
+      end
+
+    11 - digit
+  end
+
+  defp cnpj_verifying_digit_parse(digits, amount) do
+    range =
+      amount..2
+      |> Kernel.++(Enum.to_list(9..2))
+
+    digit =
+      digits
+      |> Enum.zip(range)
+      |> Enum.reduce(0, fn {d, i}, acc -> d * i + acc end)
+      |> rem(11)
+      |> case do
+        rem when rem < 2 -> 0
+        rem -> rem
+      end
+
+    11 - digit
+  end
+
+  defp validate_cpf(_, value) do
+    if String.match?(value, @cpf_format) do
+      [digits | rest] = String.split(value, "-")
+
+      digits =
+        digits
+        |> Enum.filter(&is_digit/1)
+        |> Enum.map(&String.to_integer/1)
+
+      first_verifying_digit = cpf_verifying_digit_parse(digits, 10)
+
+      first_digit =
+        rest
+        |> Enum.at(0)
+        |> String.to_integer()
+
+      second_verifying_digit =
+        digits
+        |> List.insert_at(-1, first_digit)
+        |> cpf_verifying_digit_parse(11)
+
+      rest
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.at(0)
+      |> Kernel.==(first_verifying_digit)
+      |> Kernel.&&(Enum.at(rest, 1) == second_verifying_digit)
+      |> if(do: [], else: [{:cpf, "invalid cpf"}])
+    else
+      [{:cpf, "invalid cpf format"}]
+    end
+  end
+
+  defp validate_cnpj(_, value) do
+    if String.match?(value, @cnpj_format) do
+      [digits | rest] = String.split("-")
+
+      digits =
+        digits
+        |> Enum.map(&is_digit/1)
+        |> Enum.map(&String.to_integer/1)
+
+      first_verifying_digit = cnpj_verifying_digit_parse(digits, 5)
+
+      first_digit =
+        rest
+        |> Enum.at(0)
+        |> String.to_integer()
+
+      second_verifying_digit =
+        digits
+        |> List.insert_at(-1, first_digit)
+        |> cnpj_verifying_digit_parse(11)
+
+      rest
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.at(0)
+      |> Kernel.==(first_verifying_digit)
+      |> Kernel.&&(Enum.at(rest, 1) == second_verifying_digit)
+      |> if(do: [], else: [{:cnpj, "invalid cnpj"}])
+    else
+      [{:cnpj, "invalid cnpj format"}]
+    end
   end
 
   defp execute_command(changeset, :check_password) do
