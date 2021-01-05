48eeaf9097713ffaccd817c1c376347ac4146590
Zoey <zoey.spessanha@zeetech.io>
2021-01-05 01:20:15 -0300

    authentication

---
+++ lib/bank_web/auth/error_handler.ex
@@ -0,0 +1,14 @@
+defmodule BankWeb.Auth.ErrorHandler do
+  @moduledoc """
+  Handles auth errors
+  """
+  import Plug.Conn
+
+  def auth_error(conn, {type, _reason}, _opts) do
+    body = Jason.encode!(%{message: to_string(type)})
+
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(401, body)
+  end
+end
