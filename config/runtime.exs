b81eba7f21f23c1e9c1cbc8c7b161efd241908d6
Matheus de Souza Pessanha <matheus_pessanha2001@outlook.com>
2021-01-11 12:24:09 -0300

    corrected runtime config

--- config/runtime.exs
+++ config/runtime.exs
@@ -18,33 +18,34 @@ end
 # END LOAD FROM ENV
 ################################################################################
 
-secret_key_base = load_from_env.("SECRET_KEY_BASE", nil)
+if config_env() == :prod do
+  secret_key_base = load_from_env.("SECRET_KEY_BASE", nil)
 
-app_scheme = load_from_env.("SCHEME", "https")
-app_port = String.to_integer(load_from_env.("PORT", "4000"))
-app_hostname = load_from_env.("HOST", "0.0.0.0")
-url_public = "#{app_scheme}://#{app_hostname}"
+  app_scheme = load_from_env.("SCHEME", "https")
+  app_port = String.to_integer(load_from_env.("PORT", "4000"))
+  app_hostname = load_from_env.("HOST", "0.0.0.0")
 
-db_url = load_from_env.("DB_URL", nil)
-pool_size = String.to_integer(load_from_env.("POOL_SIZE", "10"))
+  db_url = load_from_env.("DB_URL", nil)
+  pool_size = String.to_integer(load_from_env.("POOL_SIZE", "10"))
 
-config :bank, Bank.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  url: db_url,
-  pool_size: pool_size,
-  ssl: false
+  config :bank, Bank.Repo,
+    adapter: Ecto.Adapters.Postgres,
+    url: db_url,
+    pool_size: pool_size,
+    ssl: false
 
-config :bank, BankWeb.Endpoint,
-  server: true,
-  secret_key_base: secret_key_base,
-  http: [:inet6, port: app_port],
-  url: [scheme: app_scheme, host: app_hostname, port: app_port]
+  config :bank, BankWeb.Endpoint,
+    server: true,
+    secret_key_base: secret_key_base,
+    http: [:inet6, port: app_port],
+    url: [scheme: app_scheme, host: app_hostname, port: app_port]
 
-# Mailing
-# config :bank, Bank.Mailing,
-#   adapter: Bamboo.SMTPAdapter,
-#   username: "",
-#   password: "",
-#   port: 2525,
-#   server: "",
-#   hostname: ""
+  # Mailing
+  # config :bank, Bank.Mailing,
+  #   adapter: Bamboo.SMTPAdapter,
+  #   username: "",
+  #   password: "",
+  #   port: 2525,
+  #   server: "",
+  #   hostname: ""
+end
